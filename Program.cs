0edf7561c70ed6a815d317431196beb4065e677a
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-11-23 15:03:50 -0300

    Tabuada do 3 feita

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace laçofor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("valor da sua tabuada:");
+            int III = int.Parse(Console.ReadLine());
+
+            for (int i = 0;i < 10; i ++)
+            {
+                Console.WriteLine($" {III} * {i} = {III*i}");
+            }
+        }
+    }
+}
